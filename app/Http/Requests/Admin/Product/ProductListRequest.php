16285b00922ab72af5689565b784aa20296db67b
Ykutio <Ykutio@yahoo.com>
2025-08-20 01:48:51 +0400

    Added Filters, change language to english + code refactoring

--- app/Http/Requests/Admin/Product/ProductListRequest.php
+++ app/Http/Requests/Admin/Product/ProductListRequest.php
@@ -27,6 +27,7 @@ class ProductListRequest extends FormRequest
             'cat_id' => ['sometimes', 'nullable', 'integer', 'exists:categories,id'],
             'country_id' => ['sometimes', 'nullable', 'integer', 'exists:countries,id'],
             'status' => ['sometimes', 'nullable', 'string', Rule::in(array_keys(ProductStatusEnum::getProductStatusMap()))],
+            'search' => ['sometimes', 'nullable', 'string', 'min:6', 'max:20']
         ];
     }
 }
